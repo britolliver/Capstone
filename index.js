70bf03e2490feed559614c1d9bb19cd40d8a271b
Bri'Anna Tolliver <bri39annat@gmail.com>
2022-12-26 20:32:29 -0600

    working on trip display

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ import * as store from "./store";
 import Navigo from "navigo";
 import { capitalize } from "lodash";
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const router = new Navigo("/");
 
@@ -16,8 +13,13 @@ function render(state = store.Home) {
   ${Main(state)}
   ${Footer()}
   `;
+  // afterRender(state);
   router.updatePageLinks();
 }
+
+// function afterRender(state) {
+
+// }
 
 router.hooks({
   before: (done, params) => {
--- store/index.js
+++ store/index.js
@@ -4,3 +4,4 @@ export { default as Solo } from "./Solo";
 export { default as Couples } from "./Couples";
 export { default as Groups } from "./Groups";
 export { default as Links } from "./Links";
+// export { default as SoloTrips } from "./SoloTrips";
