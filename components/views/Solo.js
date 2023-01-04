165758fedd3a7b7111f0c87e4653cef9d300e954
Bri'Anna Tolliver <bri39annat@gmail.com>
2023-01-03 21:54:53 -0600

    fixed overlay issue, added more info to solo page

--- components/views/Solo.js
+++ components/views/Solo.js
@@ -110,16 +110,33 @@ export default state => html`
         </p>
       </div>
 
-      <div class="myOverlay beach" onclick="openNav()">
+      <div class="myOverlay beach" onclick="openNY()">
         <h4>New York</h4>
         <img src="${planeWindow}" />
       </div>
-      <div id="myOverlay" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
+      <div id="newYorkOverlay" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNY()"
           >&times;
         </a>
         <img src="${planeWindow}" />
         <h2>New York</h2>
+        <p>Comprised of 5 boroughs and home to Times Square, the Empire State Building, the Statue of Liberty and so much is  New York City. We've all heard of it and a lot of hearts desire to visit it. New York is known for it's night life, exquisite buildings, culture and so much more. While it is very expensive to be, I'm sure with a little finesse, a solo traveler on a budget can make it work. Keep reading for more information on things you can do.</p>
+        <h4>Lodging Ideas</h4>
+        <p>
+          Most of cost for a solo traveler will probably go towards lodging.
+          <a href="https://www.airbnb.com/" target="_blank">AirBNB</a> is a good
+          place to start looking.
+        </p>
+        <h4>Activities</h4>
+        <ul>
+        <li><a href="https://www.siferry.com/" target="_blank">Staten Island Ferry:</a>Free</li>
+        <li><a href="https://www.thehighline.org/" target="_blank">The High Line:</a> public park</li>
+        <li>Visit Times Square</li>
+        <li>Visit a museum on a free museum day</li>
+        <li>Visit art galleries</li>
+        <li>Walk around Central Park</li>
+        <li>Watch a show on Broadway</li>
+        </ul>
         <p>
           If you are visiting ${state.parks.states} and are interested in
           ${state.parks.activity}, you should visit the
@@ -127,15 +144,35 @@ export default state => html`
         </p>
       </div>
 
-      //Location name
-      <div class="myOverlay mountains" onclick="openNav()">
-        <h4>Grand Canyon</h4>
+
+      <div class="myOverlay mountains" onclick="openTulum()">
+        <h4>Tulum, Mexico</h4>
         <img src="${planeWindow}" />
       </div>
-      <div id="myOverlay" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
+      <div id="tulumOverlay" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeTulum()"
           >&times;
         </a>
+        <h2>Tulum Mexico</h2>
+        <img src="${planeWindow}" />
+        <p>Tulum is a town on the Caribbean coastline of Mexico's Yucatan Peninsula. It's known for its beaches and well-preserved ruins of an ancient Mayan port city. Lots of people take trips here.It's beautiful, fun and can definitely be done on a budget.</p>
+        <h4>Lodging Ideas</h4>
+        <p>
+        Most of cost for a solo traveler will probably go towards lodging.
+        <a href="https://www.airbnb.com/" target="_blank">AirBNB</a> is a good
+        place to start looking, but Joy Tulum is a good beach front hotel you can stay at on a budget.
+      </p>
+
+      <h4>Activities</h4>
+      <p><a href="https://luxetribes.com/" target="_blank">LuxeTribes</a> is the first place I recommend to look for this trip. It's a planning agency that has pre=planned trips with costs for solo and group travelers.They have a really nice package for a trip to <a href="tps://luxetribes.com/trips/tulum-mexico-low-budget-getaway/" target="_blank">Tulum</a></p>
+        <ul>
+        <li>Snorkeling</li>
+        <li>Go to the beach</li>
+        <li>Go on a tour- maybe of those Mayan ruins</li>
+        <li>Sailing</li>
+        <li>Check out night life</li>
+        <li>This is more of a pricey idea but it sounds cool: jungle tour</li>
+        </ul>
         <p>
           If you are visiting ${state.parks.states} and are interested in
           ${state.parks.activity}, you should visit the
@@ -143,15 +180,31 @@ export default state => html`
         </p>
       </div>
 
-      //Location name
-      <div class="myOverlay beach" onclick="openNav()">
-        <h4>Atlanta,Georgia</h4>
+      <div class="myOverlay beach" onclick="openNO()">
+        <h4>New Orleans, LA</h4>
         <img src="${planeWindow}" />
       </div>
-      <div id="myOverlay" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
+      <div id="newOrleansOverlay" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNO()"
           >&times;
         </a>
+        <h2>New Orleans, LA</h2>
+        <img src="${planeWindow}" />
+        <p>New Orleans is a Louisiana city near the Gulf of Mexico. It's know for it's nightlife, vibrant music, and especially the cuisine. It definitely has something for everybody.If you go in February, you could catch it's Mardi Gras festivities.</p>
+        <h4>Lodging Ideas</h4>
+        <p>
+        Most of cost for a solo traveler will probably go towards lodging.
+        <a href="https://www.airbnb.com/" target="_blank">AirBNB</a> is a good
+        place to start looking.
+      </p>
+
+      <ul>
+      <li><a href="https://noma.org/" target="_blank">New Orleans Museum of Art:</a> $15</li>
+      <li><a href="https://noma.org/besthoff-sculpture-garden/"> Besthoff Sculpture Garden:</a></li>
+      <li>New Orleans City Park</li>
+      <li>Try different restaurants and food native to N.O.: gumbo, beignets, etoufee,and any seafood.</li>
+      <li>Visit the French Quarter</li>
+      </ul>
         <p>
           If you are visiting ${state.parks.states} and are interested in
           ${state.parks.activity}, you should visit the
@@ -159,15 +212,31 @@ export default state => html`
         </p>
       </div>
 
-      //Location name
-      <div class="myOverlay" onclick="openNav()">
-        <h4>Location</h4>
+      <div class="myOverlay" onclick="openWashDC()">
+        <h4>Washington DC</h4>
         <img src="${planeWindow}" />
       </div>
-      <div id="myOverlay" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
+      <div id="washDCOverlay" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeWashDC()"
           >&times;
         </a>
+        <h2>Washington DC</h2>
+        <img src="${planeWindow}" />
+        <p>US Capitol. Neoclassical monuments and buildings. Iconic museums. Eclectic Neighborhoods. You won't regret a trip to Washington DC. </p>
+        <h4>Lodging Ideas</h4>
+        <p>
+        Most of cost for a solo traveler will probably go towards lodging.
+        <a href="https://www.airbnb.com/" target="_blank">AirBNB</a> is a good
+        place to start looking.
+      </p>
+      <h4>Activities</h4>
+      <ul>
+      <li>Explore Creek Park</li>
+      <li>Visit the <a href="https://www.nga.gov/" target="_blank">Natural Gallery of Art</a></li>
+      <li>Visit Roosevelt Island</li>
+      <li>Visit <a href="https://www.nps.gov/grfa/index.htm" target="_blank">Great Falls Park: </a>$10</li>
+      <li>Go ot the <a href="https://thewashingtonharbour.com/" target="_blank">Washing Harbour: </a>$20</li>
+      </ul>
         <p>
           If you are visiting ${state.parks.states} and are interested in
           ${state.parks.activity}, you should visit the
@@ -175,15 +244,33 @@ export default state => html`
         </p>
       </div>
 
-      //Location name
-      <div class="myOverlay" onclick="openNav()">
-        <h4>Location</h4>
+      <div class="myOverlay" onclick="openLA()">
+        <h4>Los Angeles, CA</h4>
         <img src="${planeWindow}" />
       </div>
-      <div id="myOverlay" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
+      <div id="laOverlay" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeLA()"
           >&times;
         </a>
+        <h2>Los Angeles, CA</h2>
+        <img src="${planeWindow}" />
+        <p>Los Angeles, or LA, is a well known city in Southern California that is also the center of America's film and television industry.There are countless reasons why people love to visit this place. Hopefully you can find a few in the activities list below. </p>
+        <h4>Lodging Ideas</h4>
+        <p>
+        Most of cost for a solo traveler will probably go towards lodging.
+        <a href="https://www.airbnb.com/" target="_blank">AirBNB</a> is a good
+        place to start looking.
+      </p>
+      <h4>Activities</h4>
+      <ul>
+      <li>Go the beach of course!</li>
+      <li>Visit some of the unique and well known food trucks.</li>
+      <li>Visit the Walk of Fame</li>
+      <li>Visit Santa Monica Pier and Beach any time of day</li>
+      <li>Visit the Getty Center</li>
+      <li>Explore Runyon Canyon Park</li>
+      <li>Walk around Sunset Boulevard or Rodeo Drive</li>
+      </ul>
         <p>
           If you are visiting ${state.parks.states} and are interested in
           ${state.parks.activity}, you should visit the
@@ -191,15 +278,29 @@ export default state => html`
         </p>
       </div>
 
-      //Location name
-      <div class="myOverlay" onclick="openNav()">
-        <h4>Location</h4>
+      <div class="myOverlay" onclick="openGrandCanyon()">
+        <h4>The Grand Canyon</h4>
         <img src="${planeWindow}" />
       </div>
-      <div id="myOverlay" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
+      <div id="grandCanyonOverlay" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeGrandCanyon()"
           >&times;
         </a>
+        <h2>The Grand Canyon</h2>
+        <img src="${planeWindow}" />
+        <p>The Grand Canyon National Park is located in Arizona and is absolutely stunning. It's layered with red rock and cannot be crossed by car. You must drive all the way around it. It's 277 river miles long and up to 18 miles wide so you can imagine it might take several trips to explore the whole canyon. </p>
+
+        <h4>Lodging Ideas</h4>
+        <p>I suggest renting a live-in van or just getting a tent to save money! There is definitely lodging there if those routes aren't appealing to you.</p>
+
+        <h4>Activities</h4>
+        <ul>
+        <li>Walk the <a href="https://tot.unm.edu/" target="_blank">Trail of Time</a></li>
+        <li>Watch the sunset at <a href="https://www.nps.gov/grca/planyourvisit/desert-view-drive.htm" target="_blank">Desert View</a></li>
+        <li>Hike the Canyon</li>
+        <li>Go on a guided tour</li>
+        <li>Bicycle the Grand Canyon</li>
+        </ul>
         <p>
           If you are visiting ${state.parks.states} and are interested in
           ${state.parks.activity}, you should visit the
@@ -207,15 +308,23 @@ export default state => html`
         </p>
       </div>
 
-      //Location name
-      <div class="myOverlay" onclick="openNav()">
-        <h4>Location</h4>
+      <div class="myOverlay" onclick="openPortland()">
+        <h4>Portland, Oregon</h4>
         <img src="${planeWindow}" />
       </div>
-      <div id="myOverlay" class="overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()"
+      <div id="portlandOverlay" class="overlay">
+        <a href="javascript:void(0)" class="closebtn" onclick="closePortland()"
           >&times;
         </a>
+        <h2>Portland, OR</h2>
+        <img src="${planeWindow}" />
+        <p>You might have heard of Portland from different documentaries whether highlighting it's unique culture, food, or houses.It's the largest city in Oregon and is also know for its parks, bridges, art scenes, bicycle paths, etc. </p>
+        <h4>Lodging Ideas</h4>
+        <p>
+        Most of cost for a solo traveler will probably go towards lodging.
+        <a href="https://www.airbnb.com/" target="_blank">AirBNB</a> is a good
+        place to start looking.
+      </p>
         <p>
           If you are visiting ${state.parks.states} and are interested in
           ${state.parks.activity}, you should visit the
