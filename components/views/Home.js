5bf1dc07197cd9dc52096f8645976bebe994a0ea
Bri'Anna Tolliver <bri39annat@gmail.com>
2023-01-20 21:16:54 -0600

    fixed homepage media query

--- components/views/Home.js
+++ components/views/Home.js
@@ -4,15 +4,15 @@ import groupLookingAtMap from "../../assets/img/groupLookingAtMap.jpg";
 import planeInTrees from "../../assets/img/planeintrees.jpg";
 
 export default state => html`
-  <section>
+  <section id="homeSection">
     <div class="logoContainer"></div>
 
     <div class="firstContainer">
-      <div class="soloImg">
-        <img class="roundBorder" src="${soloDriverInsideView}" />
-      </div>
       <div class="soloText">
         <h2>Find your next affordable adventure.</h2>
+      </div>
+      <div class="soloImg">
+        <img class="roundBorder homeImg" src="${soloDriverInsideView}" />
       </div>
     </div>
 
@@ -24,7 +24,7 @@ export default state => html`
         </h2>
       </div>
       <div class="soloImg">
-        <img class="roundBorder" src="${groupLookingAtMap}" />
+        <img class="roundBorder homeImg" src="${groupLookingAtMap}" />
       </div>
     </div>
   </section>
